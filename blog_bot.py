c908312fc35bc25541caa4dc435cea6a2aec0477
jksim0808 <dachem12@naver.com>
2026-05-24 10:55:48 +0900

    Create blog_bot.py

---
+++ blog_bot.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pyperclip
+# (생략: 로그인 및 글쓰기 로직 구현)
+# 네이버는 자동 입력 차단이 강력하므로 
+# pyperclip을 이용해 클립보드에 복사 후 붙여넣는 방식을 추천합니다.
