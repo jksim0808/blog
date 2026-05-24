10c362cca28b30b90525a97b0b1328a6425a9591
jksim0808 <dachem12@naver.com>
2026-05-24 10:58:01 +0900

    Update blog.py

--- blog.py
+++ blog.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from generator import get_blog_content
 from blog_bot import post_to_naver
+import sys
+import os
+
+# 현재 파일이 있는 디렉토리를 경로에 추가
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from blog_bot import post_to_naver
 
 st.title("네이버 블로그 자동 발행기")
 topic = st.text_input("주제를 입력하세요")
